c4e5c7ed20b0e767383b8b08443619936f16182f
ClorisYu <1055331407@qq.com>
2015-02-14 16:07:17 +0800

    Delete StdAfx.cpp

--- StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	图书管理系统.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
